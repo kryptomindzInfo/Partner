99f5d8bd84963d7f76a360e7553ea2aef0ea2c48
Adarsh Sharma <s.adarsh1996@gmail.com>
2020-09-08 21:16:59 +0530

    Added penalty to invoices

--- app/containers/CashierPayBills/PayBillsInvoiceList.js
+++ app/containers/CashierPayBills/PayBillsInvoiceList.js
@@ -7,16 +7,19 @@ import Loader from 'components/Loader';
 import Card from 'components/Card';
 import Table from '../../components/Table';
 import { STATIC_URL } from '../App/constants';
-import { checkCashierFee } from './api/CashierMerchantAPI';
+import { checkCashierFee, getPenaltyRule } from './api/CashierMerchantAPI';
 import { isEmpty } from 'lodash';
 
 const PayBillsInvoiceList = props => {
   const { merchant } = props;
+  const currentDate = new Date(); 
   const [isLoading, setLoading] = useState(true);
   const [isButtonLoading, setButtonLoading] = useState(false);
   const [selectedInvoiceList, setSelectedInvoiceList] = useState([]);
   const [totalFee, setTotalFee] = useState(0);
   const [feeList, setFeeList] = useState([]);
+  const [penaltyList, setPenaltyList] = useState([]);
+  const [penaltyRule, setPenaltyRule] = useState({});
   const [totalAmount, setTotalAmount] = useState(0);
   const [invoiceList, setInvoiceList] = useState(
     props.invoiceList.filter(i => i.paid === 0),
@@ -26,23 +29,38 @@ const PayBillsInvoiceList = props => {
     if(e.target.checked) {
       if(invoice.has_counter_invoice === true){
         const counterInvoice = invoiceList.filter((val) => val.number === `${invoice.number}C`);
-        setTotalAmount(totalAmount + invoice.amount + counterInvoice[0].amount);
+        setTotalAmount(totalAmount + invoice.amount + counterInvoice[0].amount + penaltyList[index]);
         const data = await checkCashierFee({
           merchant_id: merchant._id,
-          amount: totalAmount + invoice.amount + counterInvoice[0].amount,
+          amount: totalAmount + invoice.amount + counterInvoice[0].amount + penaltyList[index],
         });
         setTotalFee(data.fee);
-        const list = [...selectedInvoiceList,invoice._id,counterInvoice[0]._id];
+        const obj1 = {
+          id: invoice._id,
+          penalty: penaltyList[index],
+        }
+        const obj2 = {
+          id: counterInvoice[0]._id,
+          penalty: 0,
+        }
+        const list = [...selectedInvoiceList];
+        list.push(obj1);
+        list.push(obj2);
         setSelectedInvoiceList(list);
         setButtonLoading(false);
       } else {
-        setTotalAmount(totalAmount + invoice.amount);
+        setTotalAmount(totalAmount + invoice.amount + penaltyList[index]);
         const data = await checkCashierFee({
           merchant_id: merchant._id,
-          amount: totalAmount + invoice.amount,
+          amount: totalAmount + invoice.amount + penaltyList[index],
         });
         setTotalFee(data.fee);
-        const list = [...selectedInvoiceList,invoice._id];
+        const obj1 = {
+          id: invoice._id,
+          penalty: penaltyList[index],
+        }
+        const list = [...selectedInvoiceList];
+        list.push(obj1);
         setSelectedInvoiceList(list);
         setButtonLoading(false);
       }
@@ -51,22 +69,22 @@ const PayBillsInvoiceList = props => {
         const counterInvoice = invoiceList.filter((val) => val.number === `${invoice.number}C`);
         const data = await checkCashierFee({
           merchant_id: merchant._id,
-          amount: totalAmount - invoice.amount - counterInvoice[0].amount,
+          amount: totalAmount - invoice.amount - counterInvoice[0].amount - penaltyList[index],
         });
         setTotalFee(data.fee);
-        const list = selectedInvoiceList.filter((val) => val !== invoice._id &&  val !== counterInvoice[0]._id);
+        const list = selectedInvoiceList.filter((val) => val.id !== invoice._id &&  val.id !== counterInvoice[0]._id);
         setSelectedInvoiceList(list);
-        setTotalAmount(totalAmount-invoice.amount-counterInvoice[0].amount);
+        setTotalAmount(totalAmount-invoice.amount-counterInvoice[0].amount - penaltyList[index]);
         setButtonLoading(false);
       } else {
         const data = await checkCashierFee({
           merchant_id: merchant._id,
-          amount: totalAmount - invoice.amount,
+          amount: totalAmount - invoice.amount - penaltyList[index],
         });
         setTotalFee(data.fee);
-        const list = selectedInvoiceList.filter((val) => val !== invoice._id);
+        const list = selectedInvoiceList.filter((val) => val.id !== invoice._id);
         setSelectedInvoiceList(list);
-        setTotalAmount(totalAmount-invoice.amount);
+        setTotalAmount(totalAmount- invoice.amount - penaltyList[index]);
         setButtonLoading(false);
       }
     }
@@ -74,7 +92,7 @@ const PayBillsInvoiceList = props => {
 
   const handleMultipleInvoiceSubmit = () => {
     const obj = {
-      invoice_ids : selectedInvoiceList,
+      invoices : selectedInvoiceList,
       merchant_id : merchant._id,
     }
     props.showOTPPopup(obj);
@@ -129,6 +147,7 @@ const PayBillsInvoiceList = props => {
           </Row>
         </td>
         <td className="tac">{invoice.amount}</td>
+        <td className="tac">{penaltyList[index]}</td>
         <td className="tac">
           {feeList[index] > 0 ? feeList[index] : 'NA'}
         </td>
@@ -153,7 +172,7 @@ const PayBillsInvoiceList = props => {
     ));
 
   const fetchfee = async() => {
-    const feelist = invoiceList.map(async invoice => {
+    const feelist = invoiceList.map(async (invoice,index) => {
       if (invoice.amount < 0) {
         const data = await checkCashierFee({
           merchant_id: merchant._id,
@@ -163,7 +182,7 @@ const PayBillsInvoiceList = props => {
       } else {
         const data = await checkCashierFee({
           merchant_id: merchant._id,
-          amount: invoice.amount,
+          amount: invoice.amount + penaltyList[index],
         });
         return (data.fee);
       }
@@ -172,14 +191,65 @@ const PayBillsInvoiceList = props => {
     return(result);
   }
 
+  const calculatePenalty = async(rule) => {
+    const penaltylist = invoiceList.map(async invoice => {
+      const datesplit = invoice.due_date.split("/");
+      const dueDate = new Date(datesplit[2],datesplit[1],datesplit[0]);
+      if (rule.type === 'once') {
+        if( currentDate.getTime() <= dueDate.getTime()){
+          console.log("yes");
+          return (0);
+        } else {
+          console.log("no");
+          return (rule.fixed_amount + (invoice.amount*rule.percentage)/100);
+        }
+      } else {
+        if( currentDate.getTime() <= dueDate.getTime()){
+          return (0);
+        } else {
+          const diffTime = Math.abs(currentDate - dueDate);
+          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+          return ((rule.fixed_amount + invoice.amount/rule.percentage)*diffDays);
+        }
+      }
+    })
+    const result= await Promise.all(penaltylist);
+    return(result);
+  }
+
+  const fetchPenaltyRule = async() => {
+    const data = await getPenaltyRule({
+      merchant_id: merchant._id,
+    });
+    return(data.rule);
+  }
+
   useEffect(() => {
     setLoading(true);
-    const getFeeList = async () => {
-      const res= await fetchfee();
-      setFeeList(res);
+    const getRule = async() => {
+      const res1= await fetchPenaltyRule();
+      console.log(res1);
+      const res2= await calculatePenalty(res1);
+      console.log(res2);
+      setPenaltyList(res2);
+      const res3= await fetchfee();
+      setFeeList(res3);
       setLoading(false);
-    };
-    getFeeList();
+    }
+    // fetchPenaltyRule();
+    // const getPenaltyList = async () => {
+    //   const res2= await calculatePenalty();
+    //   console.log(res2);
+    //   setPenaltyList(res2);
+    // };
+    // const getFeeList = async () => {
+    //   const res3= await fetchfee();
+    //   setFeeList(res3);
+    //   setLoading(false);
+    // };
+    getRule();
+    // getPenaltyList();
+    // getFeeList();
     }, []); // Or [] if effect doesn't need props or state
   
   if (isLoading) {
@@ -207,6 +277,7 @@ const PayBillsInvoiceList = props => {
             <tr>
               <th>Number</th>
               <th>Amount</th>
+              <th>Penalty</th>
               <th>Fees</th>
               <th>Amount With Fees</th> 
               <th>Due Date</th>
