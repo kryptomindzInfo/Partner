923f23a1475c1ea9bb03d5a5fca6fd32d6e99db5
Adarsh Sharma <s.adarsh1996@gmail.com>
2021-04-03 15:36:51 +0530

    bug fix

--- app/containers/CashierReports/index.js
+++ app/containers/CashierReports/index.js
@@ -1180,7 +1180,7 @@ export default class CashierDashboard extends Component {
                   textAlign="left"
                 >
                   <thead>
-                        <tr><th>Date</th><th>Opening Time</th> <th>Opening Balance</th><th>System closing balance </th><th>Closing Balance</th><th>Discripancy</th><th>Note</th><th>Closing Time</th></tr>
+                        <tr><th>Date</th><th>Opening Time</th> <th>Opening Balance</th><th>System closing balance </th><th>Closing Balance</th><th>Discripancy</th><th>Remark</th><th>Closing Time</th></tr>
                       </thead>
                       <tbody>
                       {this.state.reports.length > 0
